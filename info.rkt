729be7eeb41f69ef8b2009ce23090f958a6cd659
Stephen De Gabrielle <spdegabrielle@users.noreply.github.com>
2023-07-01 20:57:31 +0100

    Update info.rkt to add category  "DrRacket Plugin"

--- info.rkt
+++ info.rkt
@@ -4,7 +4,7 @@
                "shlex"
                "base"))
 (define build-deps '("scribble-lib" "racket-doc"))
-(define scribblings '(("scribblings/drracket-cmdline-args.scrbl" ())))
+(define scribblings '(("scribblings/drracket-cmdline-args.scrbl" () ("DrRacket Plugin"))))
 (define pkg-desc "Command-line arguments for DrRacket")
 (define version "0.0")
 (define pkg-authors '(sorawee))
